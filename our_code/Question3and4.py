000a7632c190fa3b802beacabf31cbc1c912c046
Kelvin Encarnacao <40235904+kmencarnacao@users.noreply.github.com>
2021-03-21 13:06:49 -0500

    fixed with counting dictionary
    so we don't have to loop through the list of individual medications for a person and can just count while we initialize that person's medications.

--- our_code/Question3and4.py
+++ our_code/Question3and4.py
@@ -2,5 +2,10 @@
 def question3and4(row, individual_med_type_dict):
     if row[0] not in individual_med_type_dict:
         individual_med_type_dict[row[0]] = []
-    if row[7] not in individual_med_type_dict[row[0]]: individual_med_type_dict[row[0]] += [row[7]]
-    if row[8] not in individual_med_type_dict[row[0]]: individual_med_type_dict[row[0]] += [row[8]]
+        individual_med_type_count_dict[row[0]] = 0
+    if row[7] not in individual_med_type_dict[row[0]]:
+        individual_med_type_dict[row[0]] += [row[7]]
+        individual_med_type_count_dict[row[0]] +=1
+    if row[8] not in individual_med_type_dict[row[0]]:
+        individual_med_type_dict[row[0]] += [row[8]]
+        individual_med_type_count_dict[row[0]] +=1
