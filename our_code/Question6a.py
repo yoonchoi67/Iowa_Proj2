9562d43c3d7fc017692914c839be74ace9e37d59
Kelvin Encarnacao <40235904+kmencarnacao@users.noreply.github.com>
2021-03-29 21:53:27 -0500

    Create Question6a.py

---
+++ our_code/Question6a.py
@@ -0,0 +1,9 @@
+#create list of unique visit ids for each patient
+
+def question6a(row, individual_dict, individual_visit_count_dict):
+	if row[0] not in individual_dict:
+		individual_dict[row[0]]=[]
+		individual_visit_count_dict[row[0]]=0
+	if row[1] not in individual_dict[row[0]]:
+		individual_dict[row[0]]+=[row[1]]
+		individual_visit_count_dict[row[0]]+=1
